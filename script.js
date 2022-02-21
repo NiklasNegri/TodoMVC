f3b9a899b5df09f6d80c04481c76ae3bbd193145
NiklasNegri <69984406+NiklasNegri@users.noreply.github.com>
2022-02-21 18:04:59 +0100

    changes to html and js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const todoList = document.querySelector(".todo-list");
+const newTodoInput = document.querySelector(".new-todo-input");
+const newTodoButton = document.querySelector(".new-todo-button");
 
+newTodoButton.onclick = event => {
+    addTodo(newTodoInput.value);
+}
+
+function addTodo(todoText) {
+    let liText = document.createElement('p');
+    liText.textContent = todoText;
+
+    let liRemoveButton = document.createElement('button');
+    liRemoveButton,textContent = 'Remove';
+
+    let liElement = document.createElement('li');
+    liElement.append(liText);
+    liElement.append(liRemoveButton);
+
+    todoList.append(liElement);
+
+    liRemoveButton.onclick = event => {
+        todoList.remove(liElement);
+    }
+}
