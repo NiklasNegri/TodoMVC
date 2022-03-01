d9e6109ff921474e3936a9908734b0265bc303d6
Oscar Fundberg <oscar.fundberg@hotmail.com>
2022-03-01 16:13:16 +0100

    Changed html
    created the skeleton for the programs html

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+let form = document.querySelector('.new-todo');
+let todoList = document.querySelector('.todo-list');
+let todoInput = document.querySelector('.add-todo-input');
+
+
+
+form.onsubmit = event => {
+    event.preventDefault();
+
+    addTodo(todoInput.value)
+}
+
+function addTodo(todoText) {
+    let liCheckbox = document.createElement('input');
+    liCheckbox.type = 'checkbox';
+
+    let liText = document.createElement('p');
+    liText.textContent = todoText;
+
+    let liRemoveButton = document.createElement('button');
+    liRemoveButton.type = 'button';
+    liRemoveButton.textContent = '❌';
+
+    let liElement = document.createElement('li');
+    liElement.append(liCheckbox);
+    liElement.append(liText);
+    liElement.append(liRemoveButton);
+
+    todoList.append(liElement);
+
+    liRemoveButton.onclick = event => {
+        liElement.remove();
+    }
+    todoInput.value = '';
+}
 
